c9fd3f72d2c704a9758f734cbd963e7f24c22c7a
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-10-01 19:01:16 -0300

    add lambda

---
+++ lambda/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "lambda_authorizer" {
+  function_name = "lambda_authorizer"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+
+  filename      = "../lambda_function.zip"  
+
+  source_code_hash = filebase64sha256("../lambda_function.zip")
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
