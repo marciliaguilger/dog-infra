7f37a585247b73b1d71f5af7ca595a905971ee3b
Marcilia Guilger <51389822+marciliaguilger@users.noreply.github.com>
2024-10-01 21:01:39 -0300

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -24,7 +24,6 @@ resource "aws_iam_role" "lambda_role" {
     ]
   })
 }
-
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
